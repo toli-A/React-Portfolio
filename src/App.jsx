a9545941c1a4c10a66f79a674af6b96cb2e745d6
toli-A <anatoliabdulahi@outlook.com>
2025-02-18 12:31:25 -0600

    alright dude x2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router"
 import Header from "./components/Header.jsx"
-import Footer from "./components/footer.jsx"
+import Footer from "./components/Footer.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
